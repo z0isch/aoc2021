1b1b53831895d25dac994fcf0ff33672a3e93b0d
AJ Ruf <z0isch@gmail.com>
2021-12-02 23:18:04 -0500

    template

---
+++ src/Days/DayX.hs
@@ -0,0 +1,26 @@
+module Days.DayX
+  ( part1
+  , part2
+  , test1
+  ) where
+
+import           RIO
+
+--- >>> test1
+test1 = parseInput ""
+
+-- >>> solve1 test1
+solve1 = undefined
+
+-- >>> solve2 test1
+solve2 = undefined
+
+parseInput = undefined
+
+-- >>> part1
+part1 :: IO Integer
+part1 = solve1 . parseInput <$> readFileUtf8 "./input/dayx.txt"
+
+-- >>> part2
+part2 :: IO Integer
+part2 = solve2 . parseInput <$> readFileUtf8 "./input/dayx.txt"
